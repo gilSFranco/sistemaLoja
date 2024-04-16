fe4d776b4778ba461ba3621d4817349def5dd70c
DSM3 <fatec-dsm3@academico.intra>
2024-04-15 21:33:30 -0300

    Atualizacoes foram feitas

--- sistemaLoja/controllers/ClientesController.js
+++ sistemaLoja/controllers/ClientesController.js
@@ -2,12 +2,45 @@ import express from "express"
 const router = express.Router()
 import ClienteService from "../service/ClienteService.js"
 
-router.get("/Clientes", (req, res) => {
+router.get("/clientes", (req, res) => {
     ClienteService.selectAll().then(cliente => {
         res.render("clientes", {
             clientes: cliente
         })
     })
+})
+
+router.post("/clientes/new", (req, res) => {
+    const { nome, cpf, rua, numero, bairro, cidade } = req.body
+    ClienteService.Create(nome, cpf, rua, numero, bairro, cidade)
+
+    res.redirect('/clientes')
+})
+
+router.get("/clientes/delete/:id", (req, res) => {
+    const id = req.params.id
+    ClienteService.Delete(id)
+
+    res.redirect("/clientes")
+})
+
+router.get("/clientes/edit/:id", (req, res) => {
+    const id = req.params.id
+
+    ClienteService.selectOne(id).then(cliente => {
+        res.render("clienteEdit", {
+            cliente: cliente
+        })
+    })
+})
+
+router.post("/clientes/update/:id", (req, res) => {
+    const id = req.params.id
+    const { nome, cpf, rua, numero, bairro, cidade } = req.body
+
+    ClienteService.Update(id, nome, cpf, rua, numero, bairro, cidade)
+
+    res.redirect("/clientes")
 })
 
 export default router
--- sistemaLoja/service/ClienteService.js
+++ sistemaLoja/service/ClienteService.js
@@ -8,6 +8,47 @@ class ClienteService{
         const clientes = Cliente.find()
         return clientes
     }
+    
+    selectOne(id){
+        const cliente = Cliente.findOne({ _id: id })
+        return cliente
+    }
+
+    Create(nome, cpf, rua, numero, bairro, cidade){
+        const novoCliente = new Cliente({
+            nome: nome,
+            cpf: cpf,
+            rua: rua,
+            numero: numero,
+            bairro: bairro,
+            cidade: cidade
+        })
+
+        novoCliente.save()
+    }
+
+    Delete(id){
+        Cliente.findByIdAndDelete(id).then(() => {
+            console.log(`Cliente com a id: ${id} foi deletado com sucesso.`)
+        }).catch(erro => {
+            console.log(erro)
+        })
+    }
+
+    Update(id, nome, cpf, rua, numero, bairro, cidade){
+        Cliente.findByIdAndUpdate(id, {
+            nome: nome,
+            cpf: cpf,
+            rua: rua,
+            numero: numero,
+            bairro: bairro,
+            cidade: cidade
+        }).then(() => {
+            console.log(`Cliente com a id: ${id} foi alterado com sucesso.`)
+        }).catch(erro => {
+            console.log(erro)
+        })
+    }
 }
 
 export default new ClienteService()
